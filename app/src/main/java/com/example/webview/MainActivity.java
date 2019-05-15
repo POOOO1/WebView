4c4df1225e5595d4afae8091a1b2ecd977711e7f
polina <p-leonova@list.ru>
2019-05-15 14:25:17 +0300

    переход по ссылке и webview

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                WebView browser=(WebView)findViewById(R.id.webBrowser);
+                browser.loadUrl("https://www.metanit.com/");
+                browser.getSettings().setJavaScriptEnabled(true);
+            }
+        }
+
